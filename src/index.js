4d539dacc753069e78dd51e44b19e2c670505f2b
Polina <polina.koroleva@gmail.com>
2017-02-27 00:49:30 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getBase(base) {
-  // your solution
+	var newBase = base.split("").reduce((acc, item) => Math.max(acc, item));
+	if (newBase == 1) {
+		return base.slice(1).split("").fill("9").join("");
+	}
+	
+	for (var i = 2; i < +base; i++) {
+	  	var conversion = (+base).toString(i);
+	  	var sameLenString = "";
+	  	for (var j = 0; j < conversion.length; j++) {
+	  		sameLenString += "1";
+	  	}
+	  	if (conversion == sameLenString)
+	  		return i;
+ 	}
 }
